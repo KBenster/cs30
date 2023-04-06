96f6f50e54b0f8bb78ebb1af790e6667f0b0eede
Ben <38230235+KBenster@users.noreply.github.com>
2023-04-06 13:03:45 -0600

    The greatest IM HIM performance in human history

--- lidar/sketch.js
+++ lidar/sketch.js
@@ -13,8 +13,6 @@ function setup() {
                                 createVector(0, 100, 0), //bottom right
                                 createVector(50, 50, 0)));//top middle
   ray = {a: createVector(20, 50, -250), b:createVector(50, 50, 250)};
-
-  console.log(lineTriangleIntersect(ray, triangles[0]));
 }
 
 function draw() {
@@ -35,6 +33,7 @@ function draw() {
   vertex(triangles[0].c.x, triangles[0].c.y, triangles[0].c.z);
   endShape();
   pop();
+  lineTriangleIntersect(ray, triangles[0]);
 }
 
 class Player {
@@ -76,7 +75,19 @@ function lineTriangleIntersect(lineSegment, triangle) {
 
   if (signedVolume(q2,p1,p2,p3) === signedVolume(q1,p1,p2,p3) * -1) {
     if ((signedVolume(q1,q2,p1,p2) >= 0 === signedVolume(q1,q2,p2,p3) >= 0) && signedVolume(q1,q2,p1,p2) >= 0 === signedVolume(q1,q2,p3,p1) >= 0) {
-      return true;
+
+      let N = vectorSub(p2, p1).cross(vectorSub(p3, p1));
+      let t = -(vectorSub(q1, p1).dot(N))/(vectorSub(q2, q1).dot(N));
+      let i = vectorAdd(q1, vectorMult(t, vectorSub(q2, q1)));
+
+      push();
+      noStroke();
+      translate(i.x, i.y, i.z);
+      fill(255, 0, 0);
+      sphere(10);
+      pop();
+
+      return i;
     }
   }
 
@@ -89,4 +100,9 @@ function vectorAdd(a, b) {
 
 function vectorSub(a, b) {
   return createVector(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+function vectorMult(a, b) { // a would be a scalar hypothetically
+  let vect = b.copy();
+  return vect.mult(a);
 }
