bde83ef11d21edc7822500ae4018074a03078113
Ben <38230235+KBenster@users.noreply.github.com>
2023-04-18 09:45:44 -0600

    a

--- lidar/sketch.js
+++ lidar/sketch.js
@@ -9,7 +9,7 @@ let triangles;
 let player;
 
 const RENDER_DISTANCE = 1000; // maximum ray distance
-const RENDER_QUALITY = 1000; //number of rays cast; square this number to get the total number of rays
+const RENDER_QUALITY = 30; //number of rays cast; square this number to get the total number of rays
 const MODE = false; //true/false for webgl/camera view
 function setup() {
   if (MODE) {
